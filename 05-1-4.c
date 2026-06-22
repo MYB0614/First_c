46602bbd829f27068c5db8211ec5df8d2d8cc6c0
MYB0614 <k01051230954@gmail.com>
2026-06-22 17:43:55 +0900

    Create 05-1-4.c
    정수를 입력받아 char 자료형을 이용하여 문자로 표현하기

---
+++ 05-1-4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+    int main(void){ 
+        int num;
+        printf("문자로 바꿀 정수를 입력하시오.");
+        scanf("%d",&num);
+        printf("%c",num);
+        return 0;
+        
+        
+    }
