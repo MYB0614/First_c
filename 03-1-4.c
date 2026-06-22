ee31b748eba4b454fd96fa34445706de0f4f840b
MYB0614 <k01051230954@gmail.com>
2026-06-22 15:32:27 +0900

    Create 03-1-4.c

---
+++ 03-1-4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    int num1, num2;
+    printf("두 정수 입력 \n");
+    scanf("%d %d",&num1,&num2);
+    printf("몫 : %d, 나머지 : %d",num1/num2,num1%num2);
+    return 0;
+}
