ba26fc03b5742ef04fd962d252904e4bcd8a1db7
MYB <k01051230954@gmail.com>
2026-06-27 19:11:39 +0900

    Update Ex_1.c

--- 열혈c/challenge_1/Ex_1.c
+++ 열혈c/challenge_1/Ex_1.c
@@ -1,1 +1,14 @@
+// Ex.1) 10진수 정수를 입력 받아서 16진수로 출력하는 프로그램 작성
 
+#include <stdio.h>
+
+int main(void){
+    int num;
+    printf("10진수 정수를 입력 : ");
+    scanf("%d",&num);
+    printf("\n");
+    
+    printf("10진수 -> 16진수 변환 : %#X",num);
+    
+    return 0;
+}
