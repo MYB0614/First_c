d151a40bb04fd3692ca8dc87f7c29247af892819
MYB <k01051230954@gmail.com>
2026-07-15 19:57:37 +0900

    Create 16-1-1.c

---
+++ 열혈c/예제/16-1-1.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+
+int main(void){
+    int arr[3][9] = {0};
+    
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 9; j++){
+            arr[i][j] = (i+2)*(j+1);
+        }
+    }
+    
+    for (int i = 0 ; i < 3; i++){
+        for(int j = 0 ; j < 9; j++){
+            printf("arr[%d][%d] = %d\n", i, j, arr[i][j]);
+        }
+        printf("----------------");
+    }
+    
+    return 0;
+}
